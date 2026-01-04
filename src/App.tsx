b04d1ed78cbc997c3fd0161f6bbc43e011a2a751
Octavio <octaviofp2003@outlook.com>
2026-01-04 19:47:08 -0300

    aula 18: heading

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import { Heading } from './components/Heading';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
     return (
         <>
-            <Heading>Olá, mundo!</Heading>
+            <Heading>
+                Olá, mundo!
+                <button>
+                    <TimerIcon />
+                </button>
+            </Heading>
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
                 eaque perferendis omnis esse provident animi, numquam, suscipit
